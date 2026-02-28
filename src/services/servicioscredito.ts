5f908fb3caa71f9349e575b34ecf0cb8f868356c
Anisan <animan667@gmail.com>
2026-02-27 21:55:33 -0600

    url api creditos

--- src/services/servicioscredito.ts
+++ src/services/servicioscredito.ts
@@ -1,4 +1,4 @@
 export const consulta_creditos = async () => {
-  const res = await fetch('http://localhost:3000/api/clientes/credito/general')
+  const res = await fetch(`${import.meta.env.VITE_API_URL}clientes/credito/general`)
   return res.json()
 }
