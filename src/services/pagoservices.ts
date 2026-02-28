0ee5605e79378de80f970e2d44c98793bbca94ea
Anisan <animan667@gmail.com>
2026-02-27 22:00:20 -0600

    fix pago services

--- src/services/pagoservices.ts
+++ src/services/pagoservices.ts
@@ -1,5 +1,5 @@
 export const registrarPago = async (creditID: number, fechaPago: Date, montoDePago: number, metodoPago: string) => {
-  const res = await fetch('http://localhost:3000/api/clientes/pago', {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}clientes/pago`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
